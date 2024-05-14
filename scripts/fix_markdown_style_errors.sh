b1e823d0d3994410c795063268e1d3d3ff8061aa
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-05-14 12:39:32 +0200

    Revert "Stash"
    This reverts commit 22495cd1d4d2bd289fa3870bd660262102532f03.

--- scripts/fix_markdown_style_errors.sh
+++ scripts/fix_markdown_style_errors.sh
@@ -20,13 +20,9 @@ if [[ "$PWD" =~ scripts$ ]]; then
 fi
 
 <<<<<<< HEAD
-<<<<<<< HEAD
 markdownlint --fix 'docs/**/*.md' --ignore node_modules 
 
 # ./scripts/remove_trailing_spaces.sh
-=======
-./scripts/remove_trailing_spaces.sh
->>>>>>> parent of 3b3662f (Fix markdown)
 =======
 ./scripts/remove_trailing_spaces.sh
 >>>>>>> parent of 3b3662f (Fix markdown)
