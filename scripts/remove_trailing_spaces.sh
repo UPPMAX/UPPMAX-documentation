a3f10075cf458f0db4543fe88099fbd125a6c8ed
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-05-14 08:39:00 +0200

    Can remove all trailing whitespaces

---
+++ scripts/remove_trailing_spaces.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Remove trailing spaces of all .md files,
+# as is recommended by the Markdown checker.
+#
+# Usage:
+#
+#   ./scripts/remove_trailing_spaces.sh
+
+if [[ "$PWD" =~ scripts$ ]]; then
+    echo "FATAL ERROR."
+    echo "Please run the script from the project root. "
+    echo "Present working director: $PWD"
+    echo " "
+    echo "Tip: like this"
+    echo " "
+    echo "  ./scripts/remove_trailing_spaces.sh"
+    echo " "
+    exit 42
+fi
+
+# Go to the root folder
+cd .. || exit 41
+
+find . -type f | grep "\\.md$" | xargs -0 perl -pi -e 's/ +$//'
