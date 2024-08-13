2fe284dc0c0c4fbb4e1239f9ba97dc6710ac2496
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-08-13 13:34:00 +0200

    Script does not work

--- scripts/indent_3_to_4_spaces.sh
+++ scripts/indent_3_to_4_spaces.sh
@@ -2,13 +2,10 @@
 #
 # Indents 3 spaces to 4 spaces.
 #
-# For an indented block, a link to a broken image is ignored, as the link checker assumes it to be code.
-# In our case, it is not: for us, it is usually an admonition (i.e. a 'question' or 'info' block)
-# Here all indented text is unindented
-# https://github.com/UPPMAX/UPPMAX-documentation/issues/114
-#
 for filename in $(find . | grep .md)
 do
 # Three spaces at the start of a line (`^   `), followed by a non-space (`[^ ]`)
-sed -i 's/^   ([^ ])/ \\1/g' ${filename}
+  sed -i 's/^   ([^ ])/    \\1/g' ${filename}
 done
+
+echo "Cannot get this to work..."
