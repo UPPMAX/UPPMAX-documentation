17d90a243bf42301a5b6689c63ecb89a56337c98
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-08-13 13:32:40 +0200

    Fix script

--- scripts/indent_3_to_4_spaces.sh
+++ scripts/indent_3_to_4_spaces.sh
@@ -10,5 +10,5 @@
 for filename in $(find . | grep .md)
 do
 # Three spaces at the start of a line (`^   `), followed by a non-space (`[^ ]`)
-sed -i 's/^   ([^ ])/ \1/g' ${filename}
+sed -i 's/^   ([^ ])/ \\1/g' ${filename}
 done
