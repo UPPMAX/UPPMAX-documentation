8c806d3169db7ec1d11a86687efc4a113df701bd
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-01-08 09:19:11 +0100

    Improve doc of script

--- scripts/unindent_text.sh
+++ scripts/unindent_text.sh
@@ -7,6 +7,22 @@
 # Here all indented text is unindented
 # https://github.com/UPPMAX/UPPMAX-documentation/issues/114
 #
+# Usage:
+#
+#   ./scripts/unindent_text.sh
+
+if [[ "$PWD" =~ scripts$ ]]; then
+    echo "FATAL ERROR."
+    echo "Please run the script from the project root. "
+    echo "Present working director: $PWD"
+    echo " "
+    echo "Tip: like this"
+    echo " "
+    echo "  ./scripts/unindent_text.sh"
+    echo " "
+    exit 42
+fi
+
 for filename in $(find . | grep .md)
 do
 sed -i 's/^    //g' "${filename}"
