4079672f118ed42693fead7174c31429c130d1d7
Jayant Yadav <jayantyadav202@gmail.com>
2025-04-30 14:00:55 +0200

    setting usage limit to runllm

--- docs/js/runllm-widget.js
+++ docs/js/runllm-widget.js
@@ -8,9 +8,12 @@ document.addEventListener("DOMContentLoaded", function () {
     script.setAttribute("version", "stable");
     script.setAttribute("crossorigin", "true");
     script.setAttribute("runllm-keyboard-shortcut", "Mod+j");
-    script.setAttribute("runllm-name", "RunLLM Assistant");
+    script.setAttribute("runllm-name", "UPPMAX AI Assistant");
     script.setAttribute("runllm-position", "BOTTOM_RIGHT");
     script.setAttribute("runllm-assistant-id", "733");
+    script.setAttribute("runllm-disable-ask-a-person", "true");
+    script.setAttribute("runllm-per-user-usage-limit", "3");
+    script.setAttribute("runllm-disclaimer", "This is AI assistant is in development and may not always provide accurate information.");
   
     script.async = true;
     document.head.appendChild(script);
