61bb232d4287628094ea20a50acc5eceb40784c6
Jayant Yadav <jayantyadav202@gmail.com>
2025-04-30 13:43:46 +0200

    adding runllm configs

--- docs/js/runllm-widget.js
+++ docs/js/runllm-widget.js
@@ -8,9 +8,9 @@ document.addEventListener("DOMContentLoaded", function () {
     script.setAttribute("version", "stable");
     script.setAttribute("crossorigin", "true");
     script.setAttribute("runllm-keyboard-shortcut", "Mod+j");
-    script.setAttribute("runllm-name", "<ASSISTANT_NAME>");
-    script.setAttribute("runllm-position", "BOTTOM_RIGHT | BOTTOM_LEFT | TOP_LEFT | TOP_RIGHT");
-    script.setAttribute("runllm-assistant-id", "<ASSISTANT_ID>");
+    script.setAttribute("runllm-name", "RunLLM Assistant");
+    script.setAttribute("runllm-position", "BOTTOM_RIGHT");
+    script.setAttribute("runllm-assistant-id", "733");
   
     script.async = true;
     document.head.appendChild(script);
