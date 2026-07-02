1942b9f42e2488db18e1edd2823589865312a925
Linus Schönström <dev@lsch.se>
2026-07-02 15:56:39 +0200

    count-tags.sh: Saving the list with count

--- count-tags.sh
+++ count-tags.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 #
-# A oneliner to count the tags in the tags.json file
-jq '.[].[].tags.[] ' tags.json | sort | uniq -c | sort -n
+# A simple pipeline to count the tags in the tags.json file
+jq '.[].[].tags.[] ' tags.json |
+	sort |
+	uniq -c |
+	sort -nr > tags-counted.txt
